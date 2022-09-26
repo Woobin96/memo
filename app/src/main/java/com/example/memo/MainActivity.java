218f0cd6f81de52a184501c80833fe98aa6f49eb
woobin <hmy1219@naver.com>
2022-09-26 15:07:19 +0900

    * 기초 바텀네비게이션 작업 * 버튼 눌렀을때 인텐트 작업 * 나중에 작업할 예정 내역  - 동그라미  - 메인 화면 최초 한번 띄우고 넘어가게 하기

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.memo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected Button btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 버튼 눌렀을때 메인화면으로 이동
+        btn = findViewById(R.id.start_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), memo02.class);
+                startActivity(intent);
+            }
+        });
     }
 }
